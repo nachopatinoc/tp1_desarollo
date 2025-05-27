900c52449d4b4bfb1aadf1229620cec9230f881e
valentinoaraya <valentinoaraya04@gmail.com>
2025-05-27 15:02:47 -0300

    Modifico los modelos del prisma

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,45 +1,42 @@
-
-
 export interface UniversidadAtributos {
-  id: number;         // ID viene como <universida> en XML
-  nombre: string;     // Campo obligatorio
+    id: number;         // ID viene como <universida> en XML
+    nombre: string;     // Campo obligatorio
 }
 
-
 export interface GradoAtributos {
-  id: number;       // <grado> en XML
-  nombre: string;
+    id: number;       // <grado> en XML
+    nombre: string;
 }
 
 export interface EspecialidadAtributos {
-  id: number;         // <especialidad>
-  nombre: string;
+    id: number;         // <especialidad>
+    nombre: string;
 }
 
 export interface OrientacionAtributos {
-  id: number;             // <orientacion>
-  nombre: string;
-  especialidadId: number; // relación por ID
-  planId: number;
+    id: number;             // <orientacion>
+    nombre: string;
+    especialidadId: number; // relación por ID
+    planId: number;
 }
 
 export interface PlanAtributos {
-  id: number;       // <plan>
-  nombre: string;   // puede estar vacío
-  especialidadId: number;
+    id: number;       // <plan>
+    nombre: string;   // puede estar vacío
+    especialidadId: number;
 }
 
 export interface MateriaAtributos {
-  id: number;               // <materia>
-  nombre: string;
-  ano?: number | null;      // opcional
-  planId: number;
-  especialidadId: number;
+    id: number;               // <materia>
+    nombre: string;
+    ano?: number | null;      // opcional
+    planId: number;
+    especialidadId: number;
 }
 
 export interface LocalidadAtributos {
-  id: number;            // <codigo>
-  ciudad: string;
-  provincia: string;
-  paisDelC: string;      // <pais_del_c>
+    id: number;            // <codigo>
+    ciudad: string;
+    provincia: string;
+    paisDelC: string;      // <pais_del_c>
 }
