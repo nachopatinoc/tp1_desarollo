c8d0b0e636f49bb11a8af2006972b073376d10e1
Facundo4375 <facundocontre4375@gmail.com>
2025-05-29 18:32:41 -0300

    Agregué una modificacion en el test de universidad

--- tests/universidad.test.ts
+++ tests/universidad.test.ts
@@ -3,5 +3,5 @@ import { instanicaUniversidad as universidad } from "./utils";
 test("deberia crear una instacia de la clase universidad y leer sus atributos", () => {
     expect(universidad).toBeTruthy()
     expect(universidad.id).toBe(1)
-    expect(universidad.nombre).toBe("Universidad Nacional de Buenos Aires")
+    expect(universidad.nombre).toBe("Universidad Tegnologica Nacional de Buenos Aires")
 })
